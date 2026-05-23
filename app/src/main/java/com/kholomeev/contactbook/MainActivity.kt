af43e275bc1238e5ef0f1ed4fa39bc776110b91e
Kirill <holomeev2011@gmail.com>
2026-05-23 13:26:31 +0300

    Intent'ы

--- app/src/main/java/com/kholomeev/contactbook/MainActivity.kt
+++ app/src/main/java/com/kholomeev/contactbook/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.kholomeev.contactbook
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,9 +18,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigationevent.NavigationEventInfo
 import com.kholomeev.contactbook.ui.theme.ContactBookTheme
+import androidx.core.net.toUri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,41 +42,67 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContactBookMainMenu(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
     Column(verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()) {
-        Button(onClick = {phoneCall()}) {
+        Button(onClick = {phoneCall(context)}) {
             Text("Позвонить")
         }
-        Button(onClick = {sendEmail()}) {
+        Button(onClick = {sendEmail(context)}) {
             Text("Написать email")
         }
-        Button(onClick = {officeOnMap()}) {
+        Button(onClick = {officeOnMap(context)}) {
             Text("Показать офис на карте")
         }
-        Button(onClick = {shareContact()}) {
+        Button(onClick = {shareContact(context)}) {
             Text("Поделиться контактом")
         }
     }
 }
 
-fun phoneCall() {
-    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:+74951234567"))
-    startActivity(intent)
+fun phoneCall(context: Context) {
+    val intent = Intent(Intent.ACTION_DIAL, "tel:+74951234567".toUri())
+    if (intent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(intent)
+    }
+    else {
+        Toast.makeText(context, "Нет приложения-телефона", Toast.LENGTH_SHORT).show()
+    }
 }
 
-fun sendEmail() {
-    val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:contact@example.com"))
-    startActivity(intent)
+fun sendEmail(context: Context) {
+    val intent = Intent(Intent.ACTION_SENDTO).apply {
+        data = "mailto:contact@example.com".toUri()
+        putExtra(Intent.EXTRA_EMAIL, "contact@example.com")
+        putExtra(Intent.EXTRA_SUBJECT, "Обращение")
+    }
+    if (intent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(intent)
+    }
+    else {
+        Toast.makeText(context, "Нет почты", Toast.LENGTH_SHORT).show()
+    }
 }
 
-fun officeOnMap() {
-    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:60.0237, 30.2289"))
-    startActivity(intent)
+fun officeOnMap(context: Context) {
+    val intent = Intent(Intent.ACTION_VIEW, "geo:60.0237, 30.2289".toUri())
+    if (intent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(intent)
+    }
+    else {
+        Toast.makeText(context, "Нет карт", Toast.LENGTH_SHORT).show()
+    }
 }
 
-fun shareContact() {
-    
+fun shareContact(context: Context) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, "Контакт: +7 (495) 123-45-67, contact@example.com".toUri())
+    }
+    val chooser = Intent.createChooser(intent, "Поделиться через...")
+    context.startActivity(chooser)
 }
 
 @Preview(showBackground = true)
